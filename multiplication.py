9b79ba6fb5bd8e8f62e46d5476b854edc85553cd
eyad4education <93883982+eyad4education@users.noreply.github.com>
2021-11-21 19:28:12 +0100

    Update multiplication.py

--- multiplication.py
+++ multiplication.py
@@ -1,6 +1,7 @@
-n=int(input("Entrer un nombre entre 1 et 10: "))
-while n<1 or n>10:
+valid=False
+while valid==False:
     n=int(input("Entrer un nombre entre 1 et 10: "))
+    valid=1<=n<=10
 m=0
 for i in range(1,10):
     m=i*n
